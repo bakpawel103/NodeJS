61075316d87a95256bffea70b2c6009e6e4fd4fd
Paweł Bąk <bakp@tt.com.pl>
2019-08-13 12:07:53 +0200

    Added procfile.txt

--- server.js
+++ server.js
@@ -45,4 +45,7 @@ app.post('/users/login', async (req, res) => {
   }
 })
 
-//app.listen(2500);
+var server = app.listen(process.env.PORT || 5000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
+});
